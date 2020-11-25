d56b69d826ac9b70d3ec77406b3c1b8ecda85629
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2020-11-24 22:47:38 -0300

    new basic algorithm exercises

--- js-freeCodeCamp-full-course/06_basic-algorithm-scripting/08_truncate-a-string.js
+++ js-freeCodeCamp-full-course/06_basic-algorithm-scripting/08_truncate-a-string.js
@@ -2,42 +2,57 @@
 - Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending. */
 
 function truncateString(str, num) {
-    return str;
-}
+  let newString = str.split('');
+  let difference = str.length - num;
 
-truncateString("A-tisket a-tasket A green and yellow basket", 8);
-
-
-/* 
-function truncateString(str, num) {
-  let stringArr = str.split('');
-  let arrLength = stringArr;
-  
-  while (arrLength > num) {
-    stringArr.pop();
-    arrLength--
+  if (str.length > num) {
+    for (let i = 0; i < difference; i++) {
+      newString.pop();
+    }
+    return newString.join('') + '...';
+  };
+  if (str.length === num || str.length < num) {
+    for (let i = 0; i < difference; i++) {
+      newString.pop();
+    }
+    return newString.join('');
   }
-  return stringArr.join('')+'...';
 
 }
 
-truncateString("A-tisket a-tasket A green and yellow basket", 8);
+//truncateString("A-tisket a-tasket A green and yellow basket", 8);
 console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length));
+
+
+/* this also works:
 
 function truncateString(str, num) {
-  let stringArr = str.split('');
-  let counter = stringArr.length;
-  console.log(counter);
+  let newString = str.split('');
+  let difference = str.length - num;
 
-  if (counter > num) {
-    stringArr.pop();
-    counter--
-    return stringArr.join('')+'...';
-  } else {
-    stringArr.pop();
-    counter--
-    return stringArr.join('');
-  }
+  for (let i = 0 ; i < difference ; i++) {
+      newString.pop();
+    }
   
+  if (str.length > num) {
+    return newString.join('') + '...';
+  }; 
+  if (str.length === num || str.length < num) {
+    return newString.join('');
+  }
+
 }
+
+Resolution with slice method:
+
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
+}
+
 */
---
+++ js-freeCodeCamp-full-course/06_basic-algorithm-scripting/11_title-case-a-sentence.js
@@ -0,0 +1,17 @@
+/* Title Case a Sentence
+Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".*/
+
+function titleCase(str) {
+    let newStr = str.toLowerCase().split(' ');
+    console.log(newStr)
+    for (let i in newStr) {
+        let firstLetter = newStr[i][0].toUpperCase();
+
+    }
+    console.log(newStr)
+    return newStr.join(' ');
+}
+
+titleCase("I'm a little tea pot");
