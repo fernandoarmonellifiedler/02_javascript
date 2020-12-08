954fe304b7eb54d56b8e72a10a6dc2f402157ac5
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2020-12-08 01:09:36 -0300

    new javascristmas challenges!

---
+++ javascriptmas-challenges/06_sort-by-length.js
@@ -0,0 +1,19 @@
+/* Sort by length
+Given an array of strings, sort them in orden of increasing lengths
+*/
+
+function sortByLength(strs) {
+    let newStr = [...strs];
+    return newStr.sort((a, b) => {
+        if (a.length < b.length) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+}
+
+
+
+const strs = ["abc", "", "aaa", "a", "zz"];
+console.log(sortByLength(strs));
