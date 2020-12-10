f99ce1c76633c985976c43be4ba49e17db0d5c4d
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2020-12-10 18:15:26 -0300

    advance challenges

--- js-freeCodeCamp-full-course/09_intermediate-algorithm-scripting/04_wherefore-art-thou.js
+++ js-freeCodeCamp-full-course/09_intermediate-algorithm-scripting/04_wherefore-art-thou.js
@@ -3,23 +3,56 @@ Make a function that looks through an array of objects (first argument) and retu
 
 For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the name and its value, that was passed on as the second argument. */
 
-// INCOMPLETE SOLUTION
+// WORKING SOLUTION
 
 function whatIsInAName(collection, source) {
     var arr = [];
+
     // Only change code below this line
-    var valueToLook = source;
+    let sourceKey = Object.keys(source); // [ 'apple' ]
+    let sourceVal = Object.values(source); // [ 1 ]
 
-    for (let i = 0; i < collection.length; i++) {
+    let matchObj = collection
+        //.map(obj => Object.keys(obj))
+        .filter(obj => obj.hasOwnProperty(sourceKey))
 
-        if (collection[i].hasOwnProperty(valueToLook)) {
-            arr.push(collection[i]);
-        }
-    }
-    console.log(collection[2].hasOwnProperty(valueToLook))
-    console.log(arr)
+    console.log(matchObj);
     // Only change code above this line
     return arr;
 }
 
-whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+console.log(whatIsInAName([
+    { "apple": 1 },
+    { "apple": 1 },
+    { "apple": 1, "bat": 2 }],
+
+    { "apple": 1 }))
+
+/* WORKING SOLUTION - CASE 1
+function whatIsInAName(collection, source) {
+    var arr = [];
+
+    // Only change code below this line
+    let sourceKey = Object.keys(source); // ['last']
+    let sourceVal = Object.values(source); // ['Capulet']
+
+    for (let i = 0; i < collection.length; i++) {
+
+        if (collection[i]['last'] == sourceVal) {
+            console.log('OK!');
+            arr.push(collection[i])
+        } else {
+            console.log('NO!');
+        }
+    }
+    // Only change code above this line
+    return arr;
+}
+
+console.log(whatIsInAName([
+    { first: "Romeo", last: "Montague" },
+    { first: "Mercutio", last: null },
+    { first: "Tybalt", last: "Capulet" }],
+
+    { last: "Capulet" }));
+*/
--- js-freeCodeCamp-full-course/09_intermediate-algorithm-scripting/06_pig-latin.js
+++ js-freeCodeCamp-full-course/09_intermediate-algorithm-scripting/06_pig-latin.js
@@ -12,3 +12,6 @@ function translatePigLatin(str) {
 }
 
 translatePigLatin("consonant");
+
+
+// INCOMPLETE SOLUTION
