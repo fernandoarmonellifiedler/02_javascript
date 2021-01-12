e37f9ca776e326f28a42b8fa7e33ed94b019f00a
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2021-01-12 19:16:40 -0300

    complete new challenges

--- js-freeCodeCamp-full-course/09_intermediate-algorithm-scripting/09_missing-letters.js
+++ js-freeCodeCamp-full-course/09_intermediate-algorithm-scripting/09_missing-letters.js
@@ -4,7 +4,17 @@ Find the missing letter in the passed letter range and return it.
 If all letters are present in the range, return undefined.*/
 
 function fearNotLetter(str) {
-    return str;
+    let abcdary = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let startIndex = abcdary.indexOf(str[0]);
+    let endIndex = abcdary.length - str.length + 1;
+
+    for (let i = startIndex; i < endIndex + 1; i++) {
+        if (abcdary[i] != str[i - startIndex]) {
+            return abcdary[i];
+        }
+    }
+
+    return undefined;
 }
 
 fearNotLetter("abce");
