e157a9fa160e72d297397f02cb5e167edf736353
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2021-03-10 22:59:55 -0300

    work on solution

--- js-freeCodeCamp-full-course/10_javascript-algorithms-and-data-structures-proyects/02_roman-numeral-converter.js
+++ js-freeCodeCamp-full-course/10_javascript-algorithms-and-data-structures-proyects/02_roman-numeral-converter.js
@@ -6,70 +6,74 @@ All roman numerals answers should be provided in upper-case.
 https://www.mathsisfun.com/roman-numerals.html
 */
 
-// in progress solution
-
+// partial solution (faltan los 9 y 4)
 function convertToRoman(num) {
-    const romanNumerals = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
-    const romanArr = [1, 5, 10, 50, 100, 500, 1000];
-    let numSelected = [];
+  const romanNumerals = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+  const romanArr = [1, 5, 10, 50, 100, 500, 1000];
+  const selectedNumbers = [];
+  let newNum = num;
 
-    function determ(num) {
-        for (let i = 0; i < romanArr.length; i++) {
-            if (num > romanArr[i]) {
-                numSelected.push(romanArr[i]);
-            }
-        }
+  for (let i = 0; i < romanArr.length ; i++) {
+    if (newNum >= romanArr[i] && newNum < romanArr[i + 1]) {
+      selectedNumbers.push(romanArr[i]);
+      newNum = newNum - romanArr[i];
+      i = -1;
     }
-    determ(num);
-    console.log(numSelected)
+  }
 
+  const findIndex = selectedNumbers.map((item) => {
+    return romanArr.indexOf(item);
+  });
 
+  let convertNums = '';
+  for (let i in findIndex) {
+    convertNums += romanNumerals[findIndex[i]];
+  }
 
-    return num;
+  console.log('selected Nums: ',selectedNumbers);
+  console.log('index of selected: ', findIndex);
+  console.log(convertNums);
+  return convertNums;
 }
 
-//convertToRoman(36);
-
-console.log(convertToRoman(2))
-console.log(convertToRoman(9))
-//console.log(convertToRoman(16))
-
+convertToRoman(36);
 
 // another solution in progress
 
 function convertToRoman(num) {
-    const romanNumerals = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
-    const romanArr = [1, 5, 10, 50, 100, 500, 1000];
-    const romanObj = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
+  const romanNumerals = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+  const romanArr = [1, 5, 10, 50, 100, 500, 1000];
+  const romanObj = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let result = 0;
+  let index = 0;
+  let rest = 0;
+
+  let finalRomanNum = '';
+
+  for (let i = 0; i < romanArr.length; i++) {
+    if (num > romanArr[i]) {
+      result = romanArr[i];
+      index = i;
+      rest = num - romanArr[i];
+      finalRomanNum += romanNumerals[i];
     }
+  }
 
-    let result = 0;
-    let index = 0;
-    let rest = 0
-
-    let finalRomanNum = '';
-
-    for (let i = 0; i < romanArr.length; i++) {
-        if (num > romanArr[i]) {
-            result = romanArr[i];
-            index = i;
-            rest = num - romanArr[i];
-            finalRomanNum += romanNumerals[i];
-        }
-    }
-
-
-    console.log(result, index, rest)
-    return finalRomanNum;
+  console.log(result, index, rest);
+  return finalRomanNum;
 }
 
-console.log(convertToRoman(4))
-console.log(convertToRoman(9))
-console.log(convertToRoman(16))
+console.log(convertToRoman(4));
+console.log(convertToRoman(9));
+console.log(convertToRoman(16));
+
+
